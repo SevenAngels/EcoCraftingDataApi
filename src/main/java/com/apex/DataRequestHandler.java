ff0e9f35767b4cbd2848e63bf20f8cd91593d886
300 <aritchie05@gmail.com>
2021-05-10 00:45:14 -0500

    Add in memory response

--- src/main/java/com/apex/DataRequestHandler.java
+++ src/main/java/com/apex/DataRequestHandler.java
@@ -12,12 +12,33 @@ import static com.apex.db.DbService.*;
 
 public class DataRequestHandler implements HttpFunction {
 
+    private static final EcoDataResponse dataResponse = getDataResponse();
+
+    private static EcoDataResponse getDataResponse() {
+        try {
+            return EcoDataResponse.builder()
+                    .craftingTables(getAllCraftingTables())
+                    .upgradeModules(getAllUpgradeModules())
+                    .skills(getAllSkills())
+                    .items(getAllItems())
+                    .ingredients(getAllIngredients())
+                    .outputs(getAllOutputs())
+                    .recipes(getAllRecipes())
+                    .build();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
     @Override
     public void service(HttpRequest request, HttpResponse response) throws Exception {
         System.out.println("Received request " + request);
 
         int statusCode = 200;
 
+        /*
         EcoDataResponse dataResponse = null;
         try {
             System.out.println("Building eco data response");
@@ -34,6 +55,7 @@ public class DataRequestHandler implements HttpFunction {
             System.out.println(e);
             statusCode = 500;
         }
+        */
 
         response.setStatusCode(statusCode);
         response.appendHeader("Content-Type", "application/json");
